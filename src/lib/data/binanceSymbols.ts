93848dc67bbf8d1c1e13527ceecde180d5637aeb
r-ya23 <srm.jriya2002@gmail.com>
2025-09-26 12:57:01 +0530

    Comment out getBinanceSymbols function and related logic

--- src/lib/data/binanceSymbols.ts
+++ src/lib/data/binanceSymbols.ts
@@ -1,7 +1,7 @@
-export const getBinanceSymbols = async () => {
-  const response = await fetch("https://api.binance.com/api/v3/exchangeInfo");
-  const json = await response.json();
-  const filteredsymbols = json?.symbols.filter((s: any) => s.status == "TRADING");
-  return filteredsymbols;
-  console.log("symbols", filteredsymbols);
-};
+// export const getBinanceSymbols = async () => {
+//   const response = await fetch("https://api.binance.com/api/v3/exchangeInfo");
+//   const json = await response.json();
+//   const filteredsymbols = json?.symbols.filter((s: any) => s.status == "TRADING");
+//   return filteredsymbols;
+//   console.log("symbols", filteredsymbols);
+// };
